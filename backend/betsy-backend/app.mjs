2759883cc16af34cfa61ff3866a12da29d1cabbf
vpas <victor.passichenko@gmail.com>
2023-09-29 12:34:31 +0400

    update

--- backend/betsy-backend/app.mjs
+++ backend/betsy-backend/app.mjs
@@ -1,7 +1,22 @@
+import { 
+  DynamoDBClient, 
+  ScanCommand, 
+} from "@aws-sdk/client-dynamodb";
+
+const dbClient = new DynamoDBClient({ region: "eu-central-1" });
+
+var params = {
+  TableName: 'betsy_users',
+};
+
 export const handler = async (event, context) => {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+
+  const scanUsersCommand = new ScanCommand({TableName: "betsy_users"});
+  const response = await dbClient.send(scanUsersCommand);
+  console.log(response);
   // return context.logStreamName;
-  const body = "SPARTA";
+  const body = JSON.stringify(response.Items);
   return {
     "statusCode": 200,
     "headers": {
